dc81e70631b1337b59ed7ece59a760a2485d266f
j094097 <j094097@gmail.com>
2026-05-09 10:09:06 +0800

    修正bootstrap以及新增連結

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,5 +6,8 @@ pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 
-pin "bootstrap", to: "bootstrap.min.js", preload: true
-pin "@popperjs/core", to: "popper.js", preload: true
+# pin "bootstrap", to: "bootstrap.min.js", preload: true
+# pin "@popperjs/core", to: "popper.js", preload: true
+
+pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.3.0/dist/js/bootstrap.esm.min.js"
+pin "@popperjs/core", to: "https://unpkg.com/@popperjs/core@2.11.6/dist/esm/index.js"
